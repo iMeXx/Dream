67e431214118471028325ad0146405389db653f1
iMeXx <zimovec.maks@gmail.com>
2018-01-23 13:22:06 +0300

    1

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -44,7 +44,7 @@
 						        <span class="icon-bar"></span>
 						        <span class="icon-bar"></span>
 						      </button>
-						      <a href="/"><img src="img/logo.png" alt="" class="img-responsive logo"></a>
+						      <a href="{{ url('/') }}"><img src="img/logo.png" alt="" class="img-responsive logo"></a>
 						    </div>
 						    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						      <ul class="nav navbar-nav navbar-right">
@@ -69,8 +69,10 @@
 					<h3>стань профи в месте с нами</h3>
 					<h1>Заяви о себе</h1>
 					<p>Играй в игры, ищи напарников, делись достижениями и захватывающими моментами.</p>
-					<button class="download-btn" id="register_btn">Регистрация</button>
-					<button class="features-btn" id="login_btn">Войти</button>
+					@if (Auth::guest())
+						<button class="download-btn" id="register_btn">Регистрация</button>
+						<button class="features-btn" id="login_btn">Войти</button>
+					@endif
 				</div>
 				<div class="col-xs-6 banner-img wow fadeInRight animated">
 					<img class="img-responsive" src="img/games0.png" alt="">
@@ -202,7 +204,8 @@
 	<section class="popur_bg">
 		<div class="login_block">
 			<div class="close">&#10008;</div>
-			<form method="post" action="" class="login">
+			<form method="post" action="{{ route('login') }}" class="login">
+				{{ csrf_field() }}
 				<p>
 				  <label for="login">Логин:</label>
 				  <input type="text" name="login" id="login" value="name@example.com">
@@ -218,6 +221,40 @@
 				</p>
 
 				<p class="forgot-password"><a href="index.html">Забыл пароль?</a></p>
+			</form>
+		</div>
+	</section>
+	<!-- register form -->
+	<section>
+		<div class="popur_bg2"></div>
+		<div class="register_block">
+			<div class="close">&#10008;</div>
+			<form method="post" action="{{ route('register') }}" class="login" style="margin: 75px auto;">
+				{{ csrf_field() }}
+				<p>
+				  <label for="name">Имя:</label>
+				  <input type="text" name="name" id="name" value="{{ old('name') }}">
+				</p>
+				<p>
+				  <label for="email">E-Mail:</label>
+				  <input type="email" name="email" id="email" value="name@example.com">
+				</p>
+				@if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+				<p>
+				  <label for="password_reg">Пароль:</label>
+				  <input type="password" name="password" id="password_reg" value="4815162342">
+				</p>
+				<p>
+				  <label for="password-с">Повторите:</label>
+				  <input type="password" name="password_confirmation" id="passwor-с" value="4815162342">
+				</p>
+				<p class="login-submit">
+				  <button type="submit" class="login-button">Войти</button>
+				</p>
 			</form>
 		</div>
 	</section>
@@ -244,20 +281,34 @@
 
 		login.click(function(){
 			$('.popur_bg').css('display','block');
+
 			var window_H = $( window ).height();
 			$('.login_block').css('margin_top', window_H+'px');
 		});
 
 		$('.popur_bg').click(function(){
 			$('.popur_bg').css('display','none');
+			$('.login_block').css('display','none');
 		});
 		$('.close').click(function(){
 			$('.popur_bg').css('display','none');
+			$('.login_block').css('display','none');
 		});
 
 
 		register.click(function(){
-			alert(2);
+			$('.popur_bg2').css('display','block');
+			$('.register_block').css('display','block');
+			var window_H = $( window ).height();
+			$('.register_block').css('margin_top', window_H+'px');
+		});
+		$('.popur_bg2').click(function(){
+			$('.popur_bg2').css('display','none');
+			$('.register_block').css('display','none');
+		});
+		$('.close').click(function(){
+			$('.popur_bg2').css('display','none');
+			$('.register_block').css('display','none');
 		});
     </script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBAhzPdntTNaL7AoPevRqigGBRpWB2xhzk&callback=initMap" async defer></script>
